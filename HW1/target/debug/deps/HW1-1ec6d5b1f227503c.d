1fdeba1558ce9a2609097ad7790a4b3cf47435e7
Spiryd <neumann.maks@gmail.com>
2022-10-18 21:34:38 +0200

    initail cargo setup

---
+++ HW1/target/debug/deps/HW1-1ec6d5b1f227503c.d
@@ -0,0 +1,5 @@
+/Volumes/T7/MPiS/HW1/target/debug/deps/HW1-1ec6d5b1f227503c: src/main.rs
+
+/Volumes/T7/MPiS/HW1/target/debug/deps/HW1-1ec6d5b1f227503c.d: src/main.rs
+
+src/main.rs:
---
+++ HW1/target/debug/deps/HW1-8fd4fa27cd80b43d.d
@@ -0,0 +1,5 @@
+/Volumes/T7/MPiS/HW1/target/debug/deps/HW1-8fd4fa27cd80b43d.rmeta: src/main.rs
+
+/Volumes/T7/MPiS/HW1/target/debug/deps/HW1-8fd4fa27cd80b43d.d: src/main.rs
+
+src/main.rs:
