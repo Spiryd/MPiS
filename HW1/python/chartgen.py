b217c10fce5acc7289637fd6efba1db3090e0cb8
Spiryd <neumann.maks@gmail.com>
2022-10-26 08:08:03 +0200

    Update chartgen.py

--- HW1/python/chartgen.py
+++ HW1/python/chartgen.py
@@ -1,20 +1,24 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
-a1 = pd.read_csv("a_1.csv", header=0)
+#a1 = pd.read_csv("a_1.csv", header=0)
 #print (a1.head())
 
-a2 =pd.read_csv("a_2.csv", header=0)
+#a2 =pd.read_csv("a_2.csv", header=0)
 #print (a2.head())
 
-a3 = pd.read_csv("a_3.csv", header=0)
+#a3 = pd.read_csv("a_3.csv", header=0)
 #print (a3.head())
 
 b = pd.read_csv("b.csv", header=0)
 #print (b.head())
 
-ys = map(str, list(range(1,51)))
+bmelted = b.melt('n', var_name='cols', value_name='vals')
 
-sns.scatterplot(data = b, x = 'n' , y = 'avg')
+sns.scatterplot(data=bmelted, x='n', y = 'vals', s = 10)
+sns.scatterplot(data=b, y='avg', x='n', c = 'red')
+plt.axhline(math.pi, color = "RED")
+
 plt.show()
