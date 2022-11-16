2ea6cccc7cb4087f53c7ec421a35130d598a1bea
Spiryd <neumann.maks@gmail.com>
2022-11-16 08:47:09 +0100

    functional with saving

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,32 +1,37 @@
 import numpy as np
-import random
+import pandas as pd
+import time
 
+s_time = time.time()
 
-with open("data.csv", 'w', encoding='utf-8') as f:
-    f.write("n, B, U, L, C, D, D-C")
-    for n in range(1, 101):
-        n = n * 1000
-        for k in range(50):
-            counter = 0
-            all_no_zero = False
-            all_no_one = False
-            bins = np.zeros(n, np.uint16)
-            while(not all_no_one):
-                counter += 1
-                shot = np.random.randint(0, n-1)
-                bins[shot] += 1
-                if counter == n:
-                    u = n - np.count_nonzero(bins)#ilosc - pełne
-                    l = np.max(bins)              
-                if(not all_no_zero and np.min(bins) == 1):
-                    all_no_zero = True
-                    c = counter
-                if (np.min(bins) == 2):
-                    all_no_one = True
-                    d = counter
-                    
-                
+df = pd.DataFrame(columns=["B", "U", "L", "C", "D", "D-C"])
 
+for n in range(1, 101):
+    n = n * 1000
+    for k in range(50):
+        counter = 0
+        all_no_zero = False
+        all_no_one = False
+        bins = np.zeros(n, np.uint32)
+        row = np.zeros(6, np.uint32)
+        while(not all_no_one):
+            counter += 1
+            shot = np.random.randint(0, n)
+            bins[shot] += 1
+            if counter == n:
+                row[1] = n - np.count_nonzero(bins)#ilosc - pełne
+                row[2] = np.max(bins)              
+            if(not all_no_zero and np.min(bins) == 1):
+                all_no_zero = True
+                row[3] = counter
+                row[0] = counter + 1
+            if (np.min(bins) == 2):
+                all_no_one = True
+                row[4] = counter
+                row[5] = row[4] - row[3]
+        df.loc[len(df)] = row
+    df.to_csv(f"{n}.csv")
+            
+    e_time = time.time()    
+    print(e_time - s_time)
                 
-                
-    
