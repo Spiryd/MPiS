42348369a31ddec26b37358f8f16f83495e0eff3
Maksymilian Neumann <neumann.maks@gmail.com>
2022-11-17 16:15:03 +0100

    Update hw2.py

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -9,23 +9,31 @@ def main():
         n = n * 1000
         df = pd.DataFrame(columns=["B", "U", "L", "C", "D", "D-C"])
         for k in range(50):
-            counter = 0
             all_no_zero = False
             all_no_one = False
             bins = np.zeros(n, np.uint32)
             row = np.zeros(6, np.uint32)
+            
+            for l in range(n):
+                bins[np.random.randint(0, n)] += 1
+                row[1] = n - np.count_nonzero(bins)#ilosc - pełne
+                row[2] = np.max(bins) 
+                
+            counter = n
+            
+            if np.min(bins) == 1:
+                all_no_zero = True
+                row[3] = counter
+                row[0] = counter + 1
+            
             while(not all_no_one):
                 counter += 1
-                shot = np.random.randint(0, n)
-                bins[shot] += 1
-                if counter == n:
-                    row[1] = n - np.count_nonzero(bins)#ilosc - pełne
-                    row[2] = np.max(bins)              
+                bins[np.random.randint(0, n)] += 1  
                 if(not all_no_zero and np.min(bins) == 1):
                     all_no_zero = True
                     row[3] = counter
                     row[0] = counter + 1
-                if (np.min(bins) == 2):
+                if (all_no_zero and np.min(bins) == 2):
                     all_no_one = True
                     row[4] = counter
                     row[5] = row[4] - row[3]
