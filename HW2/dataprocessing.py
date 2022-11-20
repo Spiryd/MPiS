5a1a34ae4d1c47e21cc66d5d84f50afd7e5eef4d
Maksymilian Neumann <neumann.maks@gmail.com>
2022-11-20 12:54:48 +0100

    seperate data add avrage

---
+++ HW2/dataprocessing.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def main():
+    for col in ["B", "U", "L", "C", "D", "D-C"]:
+        cols = [str(i)  for i in range(1, 51)]
+        processed = pd.DataFrame(columns=cols)
+        for n in range(1, 101):
+            to_be_processed = pd.read_csv(f"data/{n}.csv", index_col=0)
+            to_be_processed = to_be_processed[col]
+            to_be_processed = to_be_processed.tolist()
+            processed.loc[len(processed)] = to_be_processed
+        processed.to_csv(f"processed_data/{col}.csv")
+        
+    for col in ["B", "U", "L", "C", "D"]:
+        df = pd.read_csv(f"processed_data/{col}.csv", index_col=0)
+        df.mean(axis=1).to_csv(f"processed_data/avg_{col}.csv")
+                
+if __name__ == '__main__':
+    main()
