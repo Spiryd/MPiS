dc8fc16d8da0a9eeb1cb6fcc614636acedd58158
Spiryd <neumann.maks@gmail.com>
2023-01-10 13:18:18 +0100

    update

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -6,11 +6,12 @@ import seaborn as sns
 import pandas as pd
 from numba import jit, cuda
 
+@jit
 def lcg(x, a, c, m):
     while True:
         x = (a * x + c) % m
         yield x
-        
+      
 def lcg_random_uniform_sample(n, interval, seed=0):
     a, c, m = 1103515245, 12345, 2 ** 31
     bsdrand = lcg(seed, a, c, m)
@@ -22,12 +23,15 @@ def lcg_random_uniform_sample(n, interval, seed=0):
     return sample
 
 def task1():
-    lcg_resault = lcg_random_uniform_sample(100, [0, 1])
-    pcg64 = np.random.Generator(np.random.PCG64(100))
-    pcg64_resaults = np.random.randint(0, 2, 10000) #pcg64.integers(0, 2, 10000)
-    for i in pcg64_resaults:
-        print(i, end="")
-    print(pcg64_resaults)
+    lcg_resault = lcg_random_uniform_sample(100000000, [0, 1])
+    with open('data/lcg_data.txt', 'w') as f:
+        for i in lcg_resault:
+            f.write(str(i))
+    pcg64 = np.random.Generator(np.random.PCG64())
+    pcg64_resaults = pcg64.integers(0, 2, 100000000)
+    with open('data/pcg_data.txt', 'w') as f:
+        for i in pcg64_resaults:
+            f.write(str(i))
 
 @jit
 def random_walk(n):
@@ -45,13 +49,11 @@ def task2():
     for n in N:
         print(f"N = {n}")
         column = []
-        for i in range(10000):
+        for i in range(100000):
             column.append(random_walk(n)[n-1])
         data[f'{n}'] = column
     data.set_index(data.index + 1, inplace=True)
-    print(data)
-    #sns.histplot(data['10'], cumulative=True)
-    #plt.show()
+    data.to_csv("./data/task2.csv")
 
 @jit
 def up_time(walk):
@@ -74,12 +76,31 @@ def task3():
             column.append(p)
         data[f'{n}'] = column
     data.set_index(data.index + 1, inplace=True)
-    print(data)
-           
+    data.to_csv("./data/task3.csv")
+
+def acrsin_pdf(x):
+    return(1/(np.pi*np.sqrt(x*(1 - x))))
+
+def gen_charts():
+    task3_data = pd.read_csv("./data/task3.csv", index_col=0)
+    sns.set_context("paper")
+    sns.histplot(data=task3_data, x="100", bins=20, stat="percent")
+    plt.savefig("charts/100.png")
+    plt.clf()
+    sns.histplot(data=task3_data, x="1000", bins=20, stat="percent")
+    plt.savefig("charts/1000.png")
+    plt.clf()
+    sns.histplot(data=task3_data, x="10000", bins=20, stat="percent")
+    plt.savefig("charts/10000.png")
+    plt.clf()
+    task2_data = pd.read_csv("./data/task2.csv", index_col=0)
+    sns.histplot(data=task2_data, x="5", cumulative=True)
+    plt.show()
+
     
 def main():
     title = "Choose option: "
-    options = ["Task 1", "Taks 2", "Task 3", "Exit"]
+    options = ["Task 1", "Taks 2", "Task 3", "Generate Charts", "Exit"]
     exit = False
     while not exit:
         option, index = pick(options, title, indicator='=>')
@@ -90,6 +111,8 @@ def main():
                 task2()
             case 2:
                 task3()
+            case 3:
+                gen_charts()
             case _:
                 exit = True
                 
